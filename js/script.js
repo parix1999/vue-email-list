9b260e5abafd44ff6124452bf685fddd892bfe4e
parix1999 <parisottoedoardo@gmail.com>
2021-07-08 15:43:31 +0200

    emails

--- js/script.js
+++ js/script.js
@@ -7,25 +7,30 @@ new Vue (
             emails:[],
         },
         created() {
-            getEmail();
+
+            this.getEmail();
+
+
         },
         methods:{
 
             getEmail: function() {
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                // Nelle arrow function non va la scritta function!!!
-                .then((response) => {
-                    // Devo pushare l'email all'interno della array in data:
-                    // Grazie alla arrow function posso usare .this:
-                    for (i = 0; i < 10; i++) {
+                for (i = 0; i < 10; i++) {
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    // Nelle arrow function non va la scritta function!!!
+                    .then((response) => {
+                        // Devo pushare l'email all'interno della array in data:
+                        // Grazie alla arrow function posso usare .this:
                         this.emails.push(response.data.response)
-                    };
-                });
+                        
+                    });
+                
+                }
             }
 
 
 
-            
+
         }
 
 
