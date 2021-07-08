8cf638bc45d48ab8d9e96de51c5bff1d99572948
parix1999 <parisottoedoardo@gmail.com>
2021-07-08 15:35:05 +0200

    email

--- js/script.js
+++ js/script.js
@@ -4,9 +4,29 @@ new Vue (
     {
         el:'#app',
         data: {
-            
+            emails:[],
         },
+        created() {
+            getEmail();
+        },
+        methods:{
 
+            getEmail: function() {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                // Nelle arrow function non va la scritta function!!!
+                .then((response) => {
+                    // Devo pushare l'email all'interno della array in data:
+                    // Grazie alla arrow function posso usare .this:
+                    for (i = 0; i < 10; i++) {
+                        this.emails.push(response.data.response)
+                    };
+                });
+            }
+
+
+
+            
+        }
 
 
     }
